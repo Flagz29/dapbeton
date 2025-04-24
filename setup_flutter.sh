16333b37c8305c97974161589482307dc7e4c1d4
Rifqi <rifqiiflagz@gmail.com>
2025-04-25 05:04:10 +0700

    First commit

---
+++ setup_flutter.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Install Flutter
+git clone https://github.com/flutter/flutter.git -b stable --depth 1
+export PATH="$PATH:$(pwd)/flutter/bin"
+
+# Setup dependencies
+flutter precache
+flutter config --enable-web
+flutter doctor
